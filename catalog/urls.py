fad9cb4e3d418a71f27e21af4531c792d6cef118
Darya <shelkovnikova.da@gmail.com>
2024-08-14 22:46:05 +0300

    Подготовьте два шаблона для домашней страницы и страницы с контактной информацией

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from catalog.apps import CatalogConfig
+from django.urls import path
+from catalog.views import home
+from catalog.views import contact
+
+app_name = CatalogConfig.name
+urlpatterns = [
+    path('', home, name='home'),
+    path('', contact, name='contact')
+]
